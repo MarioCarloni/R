5fa13cfcf5f08d7c2d4af28258c736455c1e6bc4
Mario Carloni <mcarloni4@gmail.com>
2016-03-07 16:35:34 -0600

    Update Redfin.R

--- Redfin.R
+++ Redfin.R
@@ -1,21 +1,67 @@
 library(dplyr)
 library(plyr)
+library(RCurl)
+library(ggplot2)
 
-setwd('C:/RedfinHousingDatasets')
+setwd('~/HouseFinder/HousingDatasets')
 
-temp <- list.files(pattern="*.csv")
+temp = list.files(pattern="*.csv")
+
 tables <- lapply(temp, read.csv)
 full <- do.call(rbind, tables)
 
 df <- full %>%
-  filter(LIST.PRICE >= 150000, LIST.PRICE <= 400000, BEDS >= 2, BATHS >= 2, !is.na(ADDRESS), HOME.TYPE %in% "Single Family Residential") %>%
-  mutate(ppsf = LIST.PRICE / SQFT) %>%
-  mutate(Address = paste(ADDRESS,CITY,STATE)) %>%
-  arrange(ppsf) %>%
-  select(Address)
+  select(ADDRESS,
+         CITY,
+         STATE,
+         LOT.SIZE,
+         LIST.PRICE,
+         SQFT,
+         LAST.SALE.DATE,
+         LAST.SALE.PRICE, 
+         HOME.TYPE, 
+         BEDS, 
+         BATHS,
+         YEAR.BUILT) %>%
+  mutate(Address = paste(ADDRESS, CITY, STATE)) %>%
+  mutate(AcreLot = LOT.SIZE * 2.2957e-5) %>%
+  mutate(ppsfcurr = LIST.PRICE / SQFT) %>%
+  mutate(ppsfprev = LAST.SALE.PRICE / SQFT) %>%
+  mutate(ppsfdiff = ppsfcurr - ppsfprev) %>%
+  mutate(lastsaledate = as.numeric(substr(LAST.SALE.DATE,1,4))) %>%
+  mutate(markup = LIST.PRICE / LAST.SALE.PRICE) %>%
+  mutate(yrsheld = 2016 - lastsaledate) %>%
+  mutate(ppsfyrinc = ppsfdiff / yrsheld) %>%
+  mutate(fiveyrnewprice = ((ppsfcurr + (ppsfyrinc * 5)) * SQFT)) %>%
+  mutate(fiveyrprof = fiveyrnewprice - LIST.PRICE) %>%
+  filter(HOME.TYPE %in% "Single Family Residential",!is.na(ppsfprev),YEAR.BUILT != 2016) %>%
+         yrsheld >= 10, 
+         fiveyrprof > 30000,
+         markup < 10,
+         YEAR.BUILT + yrsheld < 2016 - 10) %>%
+  select(Address,
+         CITY,
+         LIST.PRICE,
+         AcreLot,
+         ppsfcurr,
+         ppsfprev,
+         ppsfdiff,
+         markup,
+         yrsheld,
+         YEAR.BUILT,
+         ppsfyrinc,
+         fiveyrnewprice,
+         fiveyrprof) %>%
+  arrange(desc(fiveyrprof))
 
-addressList <- head(df$Address,10)
+addressList <- head(df$Address,15)
 
 for (i in addressList) {
   browseURL(paste0('https://www.google.com/search?q=',i))
 }
+
+qplot(yrsheld,
+      LIST.PRICE,
+      data=df, 
+      color = CITY, 
+      geom= c('point','smooth'))
