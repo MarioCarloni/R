d759b9a21d00fdde2c06686eb1ad0c2c9dcca916
Mario Carloni <mcarloni4@gmail.com>
2016-03-07 16:34:50 -0600

    Update ZipAtlas.R

--- ZipAtlas.R
+++ ZipAtlas.R
@@ -5,7 +5,7 @@ statelist <- c('ma','ri')
 
 for (i in statelist) {
 
-  url <- read_html(paste0("http://zipatlas.com/us/",i,"/city-comparison/median-household-income.htm"))
+  url <- read_html(paste0("http://zipatlas.com/us/",'ri',"/city-comparison/median-household-income.htm"))
   
   table <- url %>%
     html_nodes("table") %>% 
@@ -14,23 +14,22 @@ for (i in statelist) {
   
   table$'#' <- NULL
   table$`City Report` <- NULL
-  table$`National Rank` <- NULL
-  
-  table$Population <- as.numeric(gsub(",","", table$Population))
-  table$`Avg. Income/H/hold` <- as.numeric(gsub("\\$|,|\\.00","", table$`Avg. Income/H/hold`))
-  table$`Location (# Zip Codes)` <- gsub("\\(|\\)|[0-9]+", "", table$`Location (# Zip Codes)`)
-  
   
   table1 <- table %>% 
-    filter(`Avg. Income/H/hold` >= 70000,`Avg. Income/H/hold` <= 106000, Population >= 1000, Population <= 15000) %>%
-    mutate(percapita.index = `Avg. Income/H/hold` / Population) %>%
-    arrange(desc(percapita.index))
+    mutate(population = as.numeric(gsub(",","", Population))) %>%
+    mutate(avginchh = as.numeric(gsub("\\$|,|\\.00","", `Avg. Income/H/hold`))) %>%
+    mutate(location = gsub("\\(|\\)|[0-9]+", "", `Location (# Zip Codes)`)) %>%
+    mutate(nationalRank = as.numeric(gsub("#|,",'',`National Rank`))) %>%
+    filter(avginchh >= 80000,avginchh <= 106000, population >= 1000, population <= 21000, nationalRank < 300) %>%
+    select(location,population,avginchh,nationalRank) %>%
+    arrange(nationalRank)
   
-  citylist <- head(table1$`Location (# Zip Codes)`,10)
+  citylist <- head(table1$location,10)
   
   
-  for (loc in citylist) {
-    browseURL(paste0('https://www.google.com/#q=',loc,' redfin'))
+  for (loc in citylist) 
+    {
+    browseURL(paste0('https://www.google.com/#q=',loc,'redfin'))
   }
 
 }
